8b462bd9b53e058e298c94a9df62e70996dfc6f6
Estudio <jfsuarezb@icloud.com>
2020-05-20 19:32:42 -0500

    Checking for Arrow Function Functionality

--- src/BookComponent/BookComponent.js
+++ src/BookComponent/BookComponent.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './BookComponent.css';
 
 export default class BookComponent extends React.Component {
-    
+
+    handleClick = (_) => {
+        this.props.onClick(this.props.book);
+    }
+
     render() {
         let book = this.props.book;
         return (
             <div className="BookComponentStyle">
-                <img src={this.props.book.image} alt=""/>
+                <img src={this.props.book.image} alt="" />
                 <p className="Title">{this.props.book.name}</p>
                 <p className="Rating">{this.props.book.rating} stars</p>
                 <p>${this.props.book.price}.00</p>
